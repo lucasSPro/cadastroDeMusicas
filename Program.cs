153181569a0de3a3ccd059181eb67631b9f46eb9
Lucas dos Santos <31446385+lucasSPro@users.noreply.github.com>
2021-08-18 21:54:45 -0300

    Monta a estrutura da classe Program

--- Program.cs
+++ Program.cs
@@ -1,13 +1,58 @@
 ﻿using System;
+using CAD.Musicas.Classes;
 
 namespace CAD.Musicas
 {
     class Program
     {
+        static MusicaRepositorio repositorio =  new MusicaRepositorio();
         static void Main(string[] args)
         {
-           
-            Console.WriteLine("Hello World!");
+           string opcaoUsuario =  ObterOpcaoUsuario();
+           while(opcaoUsuario.ToUpper() != "X"){
+               switch(opcaoUsuario){
+                   case "1":
+                        ListarMusicas();
+                        break;
+                   case "2":
+                        InserirMusica();
+                        break;
+                   case "3":
+                        AtualizarMusica();
+                        break;
+                   case "4":
+                        ExcluirMusica();
+                        break;
+                   case "5":
+                        VisualizaMusica();
+                        break;
+                   case "C":
+                        Console.Clear();
+                        break;
+               }
+               opcaoUsuario = ObterOpcaoUsuario();
+           }
+           Console.WriteLine("Obrigado por utilizar nossos serviços");
+           Console.ReadLine();
+        }
+
+        private static string ObterOpcaoUsuario(){
+            return "X";
+        }
+        private static void ListarMusicas(){
+            
+        }
+        private static void InserirMusica(){
+
+        }
+        private static void AtualizarMusica(){
+
+        }
+        private static void ExcluirMusica(){
+
+        }
+        private static void VisualizaMusica(){
+
         }
     }
 }
